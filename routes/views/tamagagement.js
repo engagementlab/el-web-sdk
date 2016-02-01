29adc91cc6aedfc233864539cc3daf887d1b7e6e
jayvachon <jaylvachon@gmail.com>
2016-02-01 18:34:05 -0500

    update articles

--- routes/views/tamagagement.js
+++ routes/views/tamagagement.js
@@ -13,15 +13,39 @@
  * ==========
  */
 var keystone = require('keystone');
+var moment = require('moment');
 
 exports = module.exports = function(req, res) {
 
     var view = new keystone.View(req, res);
     var locals = res.locals;
+    // moods:
+    // sleeping, normal, bored, dead, happy, heartbroken, sad
 
     // Make any queries
     view.on('init', function(next) {
+
+        var mood = 'normal';
+        var hour = new Date().getHours();
+        
+        if (hour > 22 || hour < 9) {
+            mood = 'sleeping';
+        }
+
+        var msg = '';
+        switch(mood) {
+            case 'normal': msg = 'Errol is feeling pretty normal today!'; break;
+            case 'sleeping': msg = 'SHHH!!! Errol\'s sleeping. Keep it down silly ;)'; break;
+        }
+
+        locals.mood = mood;
+        locals.message = msg;
+
         next();
+    });
+
+    view.on('post', { action: 'tama.set_mood' }, function(next) {
+        console.log("hey :)");
     });
 
     // Render the view
