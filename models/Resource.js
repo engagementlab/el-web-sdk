a5e4c1118a7d293d73e65cd324cb6c23176fd453
Johnny Richardson <johnny.c.rich@gmail.com>
2016-02-01 18:16:59 -0500

    Fix for filename saves.

--- models/Resource.js
+++ models/Resource.js
@@ -85,7 +85,7 @@ Resource.schema.pre('save', function(next) {
 		Using filetype as the string to obtain the file extension is not 100% foolproof as it's a MIME type,
 		but it works for most common file formats. 
   */
-  if(this.file !== undefined && this.filetype !== undefined && this.file.filename === undefined) {
+  if(this.file !== undefined && this.file.filetype !== undefined && this.file.filename === undefined) {
 	
 		this.file.filename = this.key + this.file.filetype.replace('application/', '.');
 		this.file.url = this.file.url.replace('undefined', this.file.filename);
