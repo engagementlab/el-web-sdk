f692301d9ac08a49a3f76dc1447dac588aec3ca7
Johnny Richardson <johnny.c.rich@gmail.com>
2016-02-02 19:00:39 -0500

    Added route to view all projects on one page via /research/all

--- routes/views/subdirectory.js
+++ routes/views/subdirectory.js
@@ -21,6 +21,7 @@ exports = module.exports = function(req, res) {
 
     var view = new keystone.View(req, res);
     var locals = res.locals;
+    var getAll = req.params.subdirectory === 'all';
 
     // locals.section is used to set the currently selected
     locals.section = req.params.directory;
@@ -33,16 +34,21 @@ exports = module.exports = function(req, res) {
         querySub.exec(function(err, resultSub) {
 
 
-            if (resultSub === null)
+            if (resultSub === null && !getAll)
                 return res.notfound('Uh oh.', 'Sorry, but it looks like the fun stuff is missing (404)! Maybe go <a href="http://elab.emerson.edu/">back home</a>?');
 
-            locals.name = resultSub.name;
-            locals.lead = resultSub.description;
+            var projectFilter = { enabled: true };
 
-            var queryProject = Project.model.find({
-                'enabled': true,
-                'subdirectory': resultSub
-            }).sort([
+            if(!getAll) {
+                projectFilter['subdirectory'] = resultSub;
+
+                locals.name = resultSub.name;
+                locals.lead = resultSub.description;
+            }
+            else
+                locals.name = 'All Projects';
+
+            var queryProject = Project.model.find( projectFilter ).sort([
                 ['sortOrder', 'ascending']
             ]);
 
