17fc0dc9b1bd8cd5dfcf503ac53277b854d58b33
jayvachon <jaylvachon@gmail.com>
2016-02-04 13:15:20 -0500

    real tweets

--- routes/views/news.js
+++ routes/views/news.js
@@ -16,6 +16,24 @@ var Resource = keystone.list('Resource');
 var _ = require('underscore');
 var twitter = keystone.get('twitter');
 
+if (!String.prototype.splice) {
+    /**
+     * {JSDoc}
+     *
+     * The splice() method changes the content of a string by removing a range of
+     * characters and/or adding new characters.
+     *
+     * @this {String}
+     * @param {number} start Index at which to start changing the string.
+     * @param {number} delCount An integer indicating the number of old chars to remove.
+     * @param {string} newSubStr The String that is spliced in.
+     * @return {string} A new string with the spliced substring.
+     */
+    String.prototype.splice = function(start, delCount, newSubStr) {
+        return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
+    };
+}
+
 // News data propagated by ./jobs/news
 var store = require('json-fs-store')('./tmp');
 
@@ -61,10 +79,31 @@ exports = module.exports = function(req, res) {
                 locals.twitter.tweets = [];
 
                 for (var i = 0; i < tweets.length; i++) {
+                    
                     var tweet = tweets[i];
-                    locals.twitter.tweets.push({
-                        text: tweet.text
-                    });
+                    var html = tweet.text;
+                    var entities = _.union(
+                        tweet.entities.hashtags, 
+                        tweet.entities.user_mentions, 
+                        tweet.entities.urls);
+
+                    entities = _.sortBy(entities, function(n) { return n.indices[0]; }).reverse();
+
+                    for (var j = 0; j < entities.length; j++) {
+                        var e = entities[j];
+                        html = html.splice(e.indices[1], 0, "</a>");
+                        if (e.text) {
+                            // hashtag
+                            html = html.splice(e.indices[0], 0, "<a href='https://twitter.com/hashtag/" + e.text + "/'>");
+                        } else if (e.url) {
+                            // link
+                            html = html.splice(e.indices[0], 0, "<a href='" + e.url + "'>")
+                        } else if (e.screen_name) {
+                            // mention
+                            html = html.splice(e.indices[0], 0, "<a href='https://twitter.com/" + e.screen_name + "/'>")
+                        }
+                    };
+                    locals.twitter.tweets.push({ text: html });
                 }
                 next(err);
             });
