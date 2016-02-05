336b061214762d6bf76112cc274b3f7139a45cfb
Johnny Richardson <johnny.c.rich@gmail.com>
2016-02-05 11:03:57 -0500

    Syntax error fix.

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,14 +74,7 @@ module.exports = function(grunt) {
 		    /*files: [{
 		      expand: true,
 		      cwd: 'release/css',
-		      src: ['public/css/**/*.css', 
-		            'public/plugins/**/*.css',
-		            'public/fonts/**/*.css',
-		            'public/fonts/*.css',
-		            'public/bower_components/**/dist/css/*.min.css',
-		            '!public/bower_components/glidejs/dist/css/glide.theme.min.css', // We are using our own glide theme; exclude
-		            '!public/static', // Do not use any static css
-		            'public/styles/site.css'],
+		      src: [],
 		      dest: 'release/style',
 		      ext: '.min.css'
 		    }]*/
