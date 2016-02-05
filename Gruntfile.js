2f86039e6d44df43bc969ef02f34fb2f6137a6e8
Johnny Richardson <johnny.c.rich@gmail.com>
2016-02-05 11:02:54 -0500

    Grunt change.

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,8 +74,15 @@ module.exports = function(grunt) {
 		    /*files: [{
 		      expand: true,
 		      cwd: 'release/css',
-		      src: ['*.css', '!*.min.css'],
-		      dest: 'release/css',
+		      src: ['public/css/**/*.css', 
+		            'public/plugins/**/*.css',
+		            'public/fonts/**/*.css',
+		            'public/fonts/*.css',
+		            'public/bower_components/**/dist/css/*.min.css',
+		            '!public/bower_components/glidejs/dist/css/glide.theme.min.css', // We are using our own glide theme; exclude
+		            '!public/static', // Do not use any static css
+		            'public/styles/site.css'],
+		      dest: 'release/style',
 		      ext: '.min.css'
 		    }]*/
 		  }
