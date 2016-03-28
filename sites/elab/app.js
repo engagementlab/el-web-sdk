e6b4abd137b64677b5d4641a94b8b1465be6dd85
Johnny Richardson <johnny.c.rich@gmail.com>
2016-03-28 15:16:20 -0400

    Ecosystem fix, package update.

--- sites/elab/app.js
+++ sites/elab/app.js
@@ -13,11 +13,16 @@ module.exports = function() {
 
 	var keystoneInst = require('keystone');
 	var tamabehavior = require('./tamabehavior');
-	
-	keystoneInst.set('tamabehavior', tamabehavior);
 
- return { 
+	// Obtain app root path and set as keystone's module root
+	keystoneInst.set('module root', __dirname);
+
+	// lolz
+	keystoneInst.set('tamabehavior', tamabehavior);	
+
+	return { 
 		keystone: keystoneInst,
 		server: serverStart,
 		start: appStart	
-}}();
+	}
+}();
