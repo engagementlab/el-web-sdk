b9bf1d9d9f1596ecbecaf2c1bd79f3df07b3b2f9
Johnny Richardson <johnny.c.rich@gmail.com>
2016-04-04 13:35:56 -0400

    Updated uglify config to support site modules.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,7 +1,12 @@
 // Core framework-wide JS
-module.exports = {
+module.exports = function(grunt, options) {
 
-  plugins: {
+  "use strict";
+
+  var sitesArg = grunt.option('sites');
+
+  var core = {
+    
     files: {
       'public/release/core.js': 
       [
@@ -11,8 +16,51 @@ module.exports = {
         'public/plugins/*.js',  // Plugins
         'public/plugins/**/*.js',
         'public/js/*.js'
-	    ]
+      ]
+    },
+    options: {
+        preserveComments: false,
+
     }
-  }
+
+  },
+  sites = {
+      
+    files: {
+      'public/release/sites.js': []
+    },
+    options: {
+        beautify: true,
+        preserveComments: false
+      }
   
-}
+  };
+
+  // Use site modules arg only if defined
+  if(sitesArg !== undefined) {
+
+    // Site are a comma-sep list
+    var arrSites = sitesArg.replace('--sites=', '').split(',');
+
+    // Compile all site module JS to one file
+    for(var ind in arrSites) {
+     
+      sites['files']['public/release/sites.js'].push(
+
+        'node_modules/' + arrSites[ind] + '/public/bower_components/**/dist/**.min.js', // Bower
+        'node_modules/' + arrSites[ind] + '/public/bower_components/**/compiled/**.min.js', // Bower
+        'node_modules/' + arrSites[ind] + '/public/bower_components/**/js/**.min.js', // Bower
+        'node_modules/' + arrSites[ind] + '/public/plugins/*.js',  // Plugins
+        'node_modules/' + arrSites[ind] + '/public/plugins/**/*.js',
+        'node_modules/' + arrSites[ind] + '/public/js/*.js'
+
+      );
+      grunt.log.writeln("sitesArg", core);
+    
+    }
+
+  }
+
+  return {'sites': sites, 'core': core};
+
+};
