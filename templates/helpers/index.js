93a9c569e6c9e8e63137f1e36d5a2263913b41a4
Johnny Richardson <johnny.c.rich@gmail.com>
2017-03-10 13:38:58 -0500

    Added removePara HBS helper.

--- templates/helpers/index.js
+++ templates/helpers/index.js
@@ -566,5 +566,19 @@ module.exports = function() {
             return str;
     }
 
+    //  ### Remove wrapping <p> from markup html string
+    //
+    //  @str: The string
+    _helpers.removePara = function(str) {
+        
+        if(str) {
+            var re = new RegExp("<\s*p[^>]*>(.*?)<\s*/\s*p>");
+            var arr = re.exec(str);
+
+            if(arr)
+                return arr[1];
+        }
+    }
+
     return _helpers;
 };
